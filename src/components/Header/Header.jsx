cd3577dc80567a7ce72f744c02aa104a62fc0350
Lary <lary96bk@gmail.com>
2023-03-15 03:35:12 +0200

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
-import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+
 import { Link } from 'react-router-dom';
 
 const Header = () => {
@@ -14,15 +14,6 @@ const Header = () => {
       }}
     >
       <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           <Link style={{ color: 'white' }} to="/">
             HOME
